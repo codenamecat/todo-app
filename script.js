01ed6551590f50e764418ec9a90e05cdb819e536
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-03-31 15:01:59 +0300

    adding todos and counting how many are left works

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-let todos = ['Todo 1', 'Todo 2'];
+// model
+
+let todos = [{
+    title: 'Todo 1',
+    id: 'id1'
+    }, {
+    title: 'Todo 2',
+    id: 'id2'
+    }, {
+    title: 'Todo 3',
+    id: 'id3'
+    }];
+
+    // in the middle of changing these into objects instead of strings
 
 const todoListElement = document.getElementById('todo-list-element');
+const todoTitle = document.getElementById('todo-title');
+const itemsLeft = document.getElementById('items-left');
+const clearCompleted = document.getElementById('clear-completed');
+
+const allBtn = document.getElementById('all-button');
+const activeBtn = document.getElementById('active-button');
+const completedBtn = document.getElementById('completed-button');
+
+// view
 
 function renderList() {
+
+    todoListElement.innerHTML = ''; // resetting the list
+
     todos.forEach(function (todo) {
         const listItem = document.createElement('div');
-        listItem.textContent = todo;
+        listItem.textContent = todo.title;
 
         const todoCheckbox = document.createElement('input');
         todoCheckbox.setAttribute('type', 'checkbox');
@@ -19,4 +44,25 @@ function renderList() {
     });
 }
 
+function updateItemsLeft() {
+    itemsLeft.textContent = todos.length + ' items left'; //has to be unchecked items left later on
+}
+
 renderList();
+updateItemsLeft();
+
+// controller
+
+todoTitle.addEventListener('keyup', function(event) {
+    let key = event.key;
+    if (key === 'Enter') {
+        todos.push(todoTitle.value);
+        todoTitle.value = '';
+        renderList();
+        updateItemsLeft();
+    }
+})
+
+clearCompleted.addEventListener('click', function() {
+    
+})
